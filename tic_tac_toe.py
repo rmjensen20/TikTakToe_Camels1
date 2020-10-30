efeca414b193635be075e7c1d051effa7b254843
Bott26 <70854069+Bott26@users.noreply.github.com>
2020-10-29 22:37:33 -0400

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,10 +1,17 @@
 from random import *
 
-def play_tic_tac_toe():
+game_being_played = True
+winner = None
+current_player = player
 
+def play_tic_tac_toe():
+    
+    
     def init_gameboard():
         #creating board
-        gameboard=[]
+        gameboard=["-", "-", "-",
+                   "-", "-", "-",
+                   "-", "-", "-"]
         return gameboard
 
     def showgameboard(gameboard,display):
@@ -62,6 +69,110 @@ def play_tic_tac_toe():
         else:
             return "2"
         
+    def user_turn(player):
+        
+        #prompts player to input move (index 1-9)
+        position = input("Choose a position from 1-9: ")
+        position = int(position) - 1
+        
+        gameboard[position] = player
+       
+    def win_check():
+        #checks if win
+        global winner
+        
+        #check rows
+        row_winner = check_rows()
+        
+        #check columns
+        column_winner = check_columns()
+        
+        #check diagonals
+        diag_winner = check_diagonals()
+        
+        if row_winner:
+            winner = row_winner
+        elif column_winner:
+            winner = column_winner
+        elif diagonal_winner:
+            winner = diagonal_winner
+            
+         else:
+            winner = None
+         return
+        
+    def check_rows():
+        global game_being_played
+        
+        row_1 = gameboard[0] == gameboard[1] == gameboard[2] !=  "-" 
+        row_2 = gameboard[3] == gameboard[4] == gameboard[5] !=  "-"
+        row_3 = gameboard[6] == gameboard[7] == gameboard[8] !=  "-"
+        
+        if row_1 or row_2 or row_3:
+            game_being_played = False
+            
+        if row_1:
+            return gameboard[0]
+        elif row_2:
+            return gameboard[3]
+        elif row_3:
+            return gameboard[6]
+        return
+    
+    def check_columns():
+        global game_being_played
+        
+        column_1 = gameboard[0] == gameboard[3] == gameboard[6] !=  "-" 
+        column_2 = gameboard[1] == gameboard[4] == gameboard[7] !=  "-"
+        column_3 = gameboard[2] == gameboard[5] == gameboard[8] !=  "-"
+        
+        if column_1 or column_2 or column_3:
+            game_being_played = False
+            
+        if column_1:
+            return gameboard[0]
+        elif column_2:
+            return gameboard[1]
+        elif column_3:
+            return gameboard[2]
+        return       
+    
+    def check_diagonals():
+        global game_being_played
+        
+        diagonals_1 = gameboard[0] == gameboard[4] == gameboard[8] !=  "-" 
+        diagonals_2 = gameboard[6] == gameboard[4] == gameboard[2] !=  "-"
+        
+        
+        if diagonals_1 or diagonals_2:
+            game_being_played = False
+            
+        if diagonals_1:
+            return gameboard[0]
+        elif diagonals_2:
+            return gameboard[6]
+        return       
+    
+    def tie_check():
+        global game_being_played 
+        
+        #Checks if board is full and there is a tie
+        if "-" not in gameboard:
+            game_being_played = False
+        return
+    
+    def switch_player():
+        gloabal current_player
+        
+        if current_player == p1_symbol:
+            current_player = p2_symbol
+        elif current_player == p2_symbol:
+            current_player = p1_symbol
+        return
+        
+    def new_game():
+        #if win_check is true, initialize a new game
+        
     def main():
         gameboard=init_gameboard()
         introdisplay="index"
@@ -71,7 +182,17 @@ def play_tic_tac_toe():
 
         start = rand_start()
 
+        while game_being_played:
+            user_turn(current_player)
         
+            win_check():
+        
+            switch_player():
+                
+        if winner == p1_name or winner == p2_name:
+            print(winner + "won!")
+        elif winner == None:
+            print ("Tie Game.")
     main()
 
 play_tic_tac_toe()
