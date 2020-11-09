659fbc288ae0bcd9ab32f281bd93cceb927e4c6b
tylersilbey <70912835+tylersilbey@users.noreply.github.com>
2020-11-08 22:46:11 -0500

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,11 @@
+import unittest
+
+class Test(unittest.TestCase):
+    def testRandAssign(self): 
+        a
+    def testWin(self):
+        a
+    def testInput(self):
+        a
+if __name__ == '__main__':
+        unittest.main()
