f07f5e0dcd2df1af8eea5194ac363951744a7f90
jmiranda-conncoll <70863053+jmiranda-conncoll@users.noreply.github.com>
2020-11-12 17:48:02 -0500

    updated tests

--- Test.py
+++ Test.py
@@ -1,11 +1,17 @@
 import unittest
+import tic_tac_toe
 
 class Test(unittest.TestCase):
+
     def testRandAssign(self): 
-        a
+        self.
+
     def testWin(self):
-        a
+        self.assertTrue(tic_tac_toe().win_check, "Win not found")
+
     def testInput(self):
-        a
+        validInput = ["X","x","O","o"]
+        self.assertIn(tic_tac_toe().pick_symbol, validInput, "Did not pick a valid input")
+
 if __name__ == '__main__':
         unittest.main()
