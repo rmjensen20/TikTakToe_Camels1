d07d5072b5732745cafda7058a91903171d81ca8
rmjensen20 <66565526+rmjensen20@users.noreply.github.com>
2020-12-09 13:47:34 -0500

    Fixed Test.py

--- Test.py
+++ Test.py
@@ -1,33 +1,55 @@
+from random import *
 import unittest
-import tic_tac_toe
+from tic_tac_toe import check_rows
+from tic_tac_toe import check_columns
+from tic_tac_toe import check_diagonals
+from tic_tac_toe import win_check
+from tic_tac_toe import pick_symbol
+from tic_tac_toe import rand_start
 
 class Test(unittest.TestCase):
 
     def testRandAssign(self):
-        randomcheck=0  # +-10 window allowed
-        goodReturn=["1","2"]   #function is working as expected
+        randomcheck=0  # +-20 window allowed
+        goodReturn=[1,2]
         for i in range(101):
-            self.assertIn(tic_tac_toe().rand_start,goodReturn,"Did not randomly pick player")
-            if tic_tac_toe().rand_start == "1":
+            self.assertIn(rand_start(),goodReturn) #verifies that correct num chosen
+            if rand_start() == 1:   #verifies that selection is actually random
                 randomcheck=randomcheck+1
             else:
                 randomcheck=randomcheck-1
-        if not (40 >= randomcheck <= 60):
-            print("Unsuitable Randomness")
+        if randomcheck <= 20 and randomcheck >= -20:
+            print("Suitable Randomness found,",randomcheck)
+        else:
+            print("Unsuitable Randomness found,",randomcheck)
+            
                 
     def testWin(self):
-        Names = tic_tac_toe.get_names
+        Names = ['Player1','Player2']
         #These return the name of the winner
-        self.assertIn(tic_tac_toe.check_rows, Names, "Win not found in rows")
-        self.assertIn(tic_tac_toe.check_columns, Names, "Win not found in columns")
-        self.assertIn(tic_tac_toe.check_diagonals, Names, "Win not found in diagonals")
-        self.assertTrue(tic_tac_toe().win_check, "Win not found")
+
+        #executes check_row on a board that has winning row
+        gameboard=["X","X","X","-","-","-","-","-","-"]
+        self.assertIn(check_rows(gameboard, Names,'X','O'), Names, "Win not found in rows")
+
+        #executres check_columns on a board that has a winning column
+        gameboard=["X","-","-","X","-","-","X","-","-"]
+        self.assertIn(check_columns(gameboard, Names,'X','O'), Names, "Win not found in columns")
+
+        #executes check_diagonals on a board that has a winning diagonal
+        gameboard=["X","-","-","-","X","-","-","-","X"]
+        self.assertIn(check_diagonals(gameboard, Names,'X','O'), Names, "Win not found in diagonals")
+
+        print("Win check works for rows, columns, diagonals")
 
     def testInput(self):
+        #valid symbols
         validInput = ["X","x","O","o"]
-        result = tic_tac_toe().pick_symbol
+        result = pick_symbol(['Player1','Player2'])
         for x in result:
             self.assertIn(x, validInput, "Did not pick a valid input")
+        print("Symbol selection works as intended, check misinputs properly")
 
 if __name__ == '__main__':
         unittest.main()
+
